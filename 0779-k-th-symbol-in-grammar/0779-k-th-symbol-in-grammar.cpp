a23dc00cc45338616393eb159566709fb2cb861f
Anubhav Bhardwaj <Anubhavsh24@gmail.com>
2026-07-12 15:55:45 +0530

    Time: 0 ms (100%), Space: 8 MB (12.55%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int helper(int n , int k,int a){
+    int helper(int n , int k){
         int as;
         if(n==1){
             return 0;
         }
-        if(k%2==0)as=  helper(n-1,k/2,k);
-        else as=  helper(n-1,(k+1)/2,k);
+        if(k%2==0)as=  helper(n-1,k/2);
+        else as=  helper(n-1,(k+1)/2);
         if(k%2==0) return !as;
         else return as;
     }
     
     int kthGrammar(int n, int k) {
-        int ans = helper(n,k,k);
+        int ans = helper(n,k);
         return ans;
 
     }
