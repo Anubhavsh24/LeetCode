d7186bc3ebf5f240ff24881d7868c905ea03f566
Anubhav Bhardwaj <Anubhavsh24@gmail.com>
2026-07-13 23:18:46 +0530

    Time: 0 ms (100%), Space: 8.9 MB (58.76%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        string ans="";
-        int m= a.length();
-        int n=b.length();
+        int i = a.size() - 1;
+        int j = b.size() - 1;
         int carry=0;
-        int var=min(m,n);
-        for(int i=1;i<=var;i++){
-            int sum=(a[m-i]-'0')+(b[n-i]-'0')+carry;
-            ans.push_back((sum%2)+'0');
-            carry=sum/2;
-        }
-        if(m!=n){
-            if(var==m){
-                var=n-m;
-                for(int i=var-1;i>=0;i--){
-                    int sum=(b[i]-'0')+carry;
-                    ans.push_back((sum%2)+'0');
-                    carry=sum/2;
-                }
-            }
-        
-            else{
-                var=m-n;
-                for(int i=var-1;i>=0;i--){
-                    int sum=(a[i]-'0')+carry;
-                    ans.push_back((sum%2)+'0');
-                    carry=sum/2;
-                }
-            }
+        string ans="";
+
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+
+            ans.push_back((sum % 2) + '0');
+            carry = sum / 2;
         }
 
-        if(carry==1) ans.push_back('1');
-        reverse(ans.begin(),ans.end());
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
