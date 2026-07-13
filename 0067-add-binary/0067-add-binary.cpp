a34f2baf0041aad380466ac9c4338252dfe07dda
Anubhav Bhardwaj <Anubhavsh24@gmail.com>
2026-07-13 23:15:45 +0530

    Time: 0 ms (100%), Space: 8.9 MB (80.86%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -8,44 +8,16 @@ public:
         int var=min(m,n);
         for(int i=1;i<=var;i++){
             int sum=(a[m-i]-'0')+(b[n-i]-'0')+carry;
-            if(sum==0){
-                ans.push_back('0');
-                carry=0;
-            }
-            else if(sum==1){
-                ans.push_back('1');
-                carry=0;
-            }
-            else if(sum==2){
-                ans.push_back('0');
-                carry=1;
-            }
-            else{
-                ans.push_back('1');
-                carry=1;
-            }
+            ans.push_back((sum%2)+'0');
+            carry=sum/2;
         }
         if(m!=n){
             if(var==m){
                 var=n-m;
                 for(int i=var-1;i>=0;i--){
                     int sum=(b[i]-'0')+carry;
-                    if(sum==0){
-                        ans.push_back('0');
-                        carry=0;
-                    }
-                    else if(sum==1){
-                        ans.push_back('1');
-                        carry=0;
-                    }
-                    else if(sum==2){
-                        ans.push_back('0');
-                        carry=1;
-                    }
-                    else{
-                        ans.push_back('1');
-                        carry=1;
-                    }
+                    ans.push_back((sum%2)+'0');
+                    carry=sum/2;
                 }
             }
         
@@ -53,22 +25,8 @@ public:
                 var=m-n;
                 for(int i=var-1;i>=0;i--){
                     int sum=(a[i]-'0')+carry;
-                    if(sum==0){
-                        ans.push_back('0');
-                        carry=0;
-                    }
-                    else if(sum==1){
-                        ans.push_back('1');
-                        carry=0;
-                    }
-                    else if(sum==2){
-                        ans.push_back('0');
-                        carry=1;
-                    }
-                    else{
-                        ans.push_back('1');
-                        carry=1;
-                    }
+                    ans.push_back((sum%2)+'0');
+                    carry=sum/2;
                 }
             }
         }
