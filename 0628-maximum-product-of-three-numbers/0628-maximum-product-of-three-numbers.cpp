63658a3e3ad375f6789a4386d1e71d5256d47ac0
Anubhav Bhardwaj <Anubhavsh24@gmail.com>
2026-07-26 22:00:55 +0530

    Time: 0 ms (100%), Space: 31.4 MB (96.8%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -5,7 +5,8 @@ public:
         int n=nums.size()-1;
         int ans1= (nums[n]*nums[n-1]*nums[n-2]);
         int ans2= nums[0]*nums[1]*nums[n];
-        return max(ans1,ans2);
+        if(ans1<ans2) return ans2;
+        else return ans1;
         
     }
 };
