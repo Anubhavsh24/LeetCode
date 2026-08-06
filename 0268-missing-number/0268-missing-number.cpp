9b62cccca9a0abb1df98ee81365e8a995d61d647
Anubhav Bhardwaj <Anubhavsh24@gmail.com>
2026-08-06 15:23:43 +0530

    Time: 0 ms (100%), Space: 22.3 MB (8.93%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,11 +1,16 @@
 class Solution {
-public:
+public: // By Cyclic Sort Method
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
-        int ans=n;
-        for(int x=0;x<n;x++){
-            ans^=x^nums[x];
+        vector<int> check(n+1,false);
+        for(int i=0;i<n;i++){
+            int ele=nums[i];
+            check[ele]=true;
+
         }
-        return ans;
+        for(int i=0;i<=n;i++){
+            if(check[i]==false) return i;
+        }
+        return 0;
     }
 };
